2fda096fa60b83004d7a39ed47c2a0765f79eea2
Sumesh <sumeshvasu@gmail.com>
2015-04-28 14:12:09 +0530

    Second commit with cURL

--- Crawler.php
+++ Crawler.php
@@ -9,7 +9,16 @@ class Crawler
 	
 	function getbody( $path ) 
 	{
-		$pageFullContent = file_get_contents( $path );
+		//$pageFullContent = file_get_contents( $path );
+
+
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, $path);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_HEADER, false);
+		$pageFullContent = curl_exec($curl);
+		curl_close($curl);
+
 		preg_match("/<body[^>]*>(.*?)<\/body>/is", $pageFullContent, $bodyMatches);
 		if( isset( $bodyMatches[1] ) ) {
 			return $bodyMatches[1];
@@ -27,7 +36,9 @@ class Crawler
 				$fetchedUrl = $match[2];				
 				if (preg_match('%^https?://[^\s]+$%', $fetchedUrl )) {
 					if( !in_array( $fetchedUrl,  $anchorTagArray ) ) {
-						$anchorTagArray[] = $fetchedUrl;
+						if( $this->checkIsImageUrl( $fetchedUrl ) ) {
+							$anchorTagArray[] = $fetchedUrl;
+						}
 					}
 				} else {
 			    	if($this->firstCharCheck( $fetchedUrl, '/')){
@@ -41,8 +52,19 @@ class Crawler
 		}
 		return $anchorTagArray;
 	}
-	
-	function firstCharCheck($haystack, $needle) 
+
+	function checkIsImageUrl( $url )
+	{
+		$extension = pathinfo( $url, PATHINFO_EXTENSION );
+		if ( $extension == 'jpg' || $extension == 'jpeg' || $extension == 'png' || $extension == 'gif' ) {
+			return false;
+		} else {
+			return true;
+		}
+
+	}
+
+	function firstCharCheck($haystack, $needle)
 	{
 		return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== FALSE;
 	}
